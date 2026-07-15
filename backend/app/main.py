618f81a136f4812f3d625b260dddc419507d38fd
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-07-15 16:16:55 +0530

    anable cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,10 +17,20 @@
 from fastapi import FastAPI
 
 from app.routers.prediction import router as prediction_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Rainfall Prediction API",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(prediction_router)
